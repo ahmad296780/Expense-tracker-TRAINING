201c3bc7d08d4f625ecbaab65de906312f0cd162
ahmad <bha296780@gmail.com>
2022-10-30 18:56:22 -0400

    initial

---
+++ App/Migrations/20221027043944_Initial.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Icon = table.Column<string>(type: "nvarchar(5)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryID = table.Column<int>(type: "int", nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(50)", nullable: true),
+                    Amount = table.Column<double>(type: "float", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
---
+++ App/Models/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace App.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+
+        [Range(1,int.MaxValue,ErrorMessage ="Please select a category")]
+        public int CategoryID   { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string? Note { get; set; }
+       [ Range(1, double.MaxValue, ErrorMessage = "Please enter amount")]
+        public double Amount  { get; set; }
+        public DateTime Date { get; set; }= DateTime.Now;
+    }
+}
